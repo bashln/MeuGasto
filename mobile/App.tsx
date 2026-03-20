e8c6fec604247fe9ff3d6b0beaf0a34f1199efc8
bashln <lpdmonteiro@gmail.com>
2026-03-20 13:44:48 -0300

    feat: atualiza splash, loading e ícone do app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -69,6 +69,6 @@ export default function App() {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    backgroundColor: colors.backgroundApp,
+    backgroundColor: colors.backgroundAuth,
   },
 });
